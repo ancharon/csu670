f410763445305bc1831788906ed3daddfd1644ba
napalmer <napalmer@25ffbd92-ab45-0410-b18a-27d455ed4605>
2008-02-26 21:39:52 +0000

    added gameplayer.py and moved Room and Gameover classes to graph.py. They probably shouldn't stay there, but it's convenient for now. Someday they should have a module of their own.

--- trunk/xmlparser/graph.py
+++ trunk/xmlparser/graph.py
@@ -1,10 +1,110 @@
 #!/usr/bin/env python
 
-from parser import *
+import sys, os
+import unittest
+
+#FIXME: Room and Gameover should be somewhere else (perhaps a gameelements module?)  
+class Room(object):
+    '''A single room in a castle. Parsed from input.'''
+    
+    def __init__(self):
+        #Sets up empty values for the room object
+        self.purpose = ""
+        self.characteristics = set()
+        self.exits = set()
+        return
+        
+    def initialize(self, xmlElement):
+        '''Initialize this Room's attributes using data from parsed XML element'''
+        roomProperties = xmlElement.getElements()
+        for prop in roomProperties:
+            #Assign the Room properties according to the element data
+            if prop.name == "purpose":
+                self.setPurpose(prop.cdata.strip())
+            elif prop.name == "characteristic":
+                self.addCharacteristic(prop.cdata.strip())
+            elif prop.name == "exits":
+                exitList = prop.getElements()
+                for exit in exitList:
+                    self.addExit(exit.cdata.strip())
+        
+    def setPurpose(self, purpose):
+        self.purpose = unicode(purpose)
+        return
+        
+    def getPurpose(self):
+        return self.purpose
+        
+    def addCharacteristic(self, characteristic):
+        self.characteristics.add(unicode(characteristic))
+        return
+        
+    def getCharacteristics(self):
+        return self.characteristics
+    
+    #FIXME: exits should be Edges, not strings. Use the incoming string to
+    # create an Edge.    
+    def addExit(self, exit):
+        self.exits.add(unicode(exit))
+        return
+        
+    def getExits(self):
+        return self.exits
+        
+    def isEqual(self, otherRoom):
+        if (otherRoom.purpose == self.purpose) and \
+           (self.characteristics == otherRoom.characteristics):
+            return True
+        else:
+            return False
+            
+    def isNull(self):
+        if self.purpose:
+            return False
+        else: 
+            return True
+    
+    def toString(self):
+        returnString = ""
+        returnString = u"This room is a " + self.getPurpose() + os.linesep
+        if self.getCharacteristics():
+            returnString += u"It has the following characteristics:" + os.linesep
+            for char in self.getCharacteristics():
+                returnString += char + os.linesep
+        if self.getExits():
+            returnString += u"There are exits in the following directions:" + os.linesep
+            for exit in self.getExits():
+                returnString += exit + os.linesep
+        return returnString
+        
+class Gameover(object):
+    '''A game over message. Parsed from input.'''
+
+    def __init__(self):
+        self.outcome = ""
+
+    def setOutcome(self, outcome):
+        self.outcome = unicode(outcome)
+
+    def getOutcome(self):
+        return self.outcome
+
+    def initialize(self, xmlElement):
+        gameoverProperties = xmlElement.getElements()
+        for prop in gameoverProperties:
+            #There should be only one, outcome
+            if prop.name == "outcome":
+                self.setOutcome(prop.cdata.strip())
+
+    def toString(self):
+        returnString = u"The game is over." + os.linesep
+        returnString += u"Outcome: " + self.getOutcome() + os.linesep
+        return returnString
+
 
 class Edge(object):
     
-    def __init__(self, direction, (node1=Node(), node2=Node())):
+    def __init__(self, direction, (node1, node2)):
         '''An edge means that to get from node1 to node2, you go direction'''
         self.direction = direction
         self.nodes = (node1, node2)
@@ -30,6 +130,7 @@ class Node(object):
     def getExits(self):
         return self.room.getExits()
 
+
 #FIXME: eliminate all mentions of Node, replace with Room
 class Graph(object):
 
@@ -43,7 +144,7 @@ class Graph(object):
             self.addEdge(exit, (self, Room()))
         return self.room.exits()
         
-    def addEdge(self, direction, (node1, node2=Node())):
+    def addEdge(self, direction, (node1, node2)):
         #FIXME: If there is already an edge going in this direction from node 1 to a non-null node,
         # then don't add anything. Otherwise, add this new edge.
         self.edges.add(Edge(direction, (node1, node2)))
@@ -52,35 +153,50 @@ class Graph(object):
         self.rooms.add(room)
         self.addEdges(room)
         
-#FIXME: This class should be in another file
-#It should keep track of its last move, last room, and current room so that
-# it can pass them to the graph as necessary.
-class GamePlayer(object):
-    
-    def __init__(self):
-        self.graph = Graph()
-        self.currentRoom = None
-        self.previousRoom = None
-        self.lastMove = None
-        
-    def move(self, direction):
-        #TODO: write move to stdout
-        #do the move and save it to self.lastMove.
-        myParser = gameparser.Xml2Obj()
-        specname = os.path.join('xml', 'relaxng-hw5.rng')
-        element = myParser.Parse(None, specname)
-        if type(element) == Gameover:
-            print element.outcome
-        elif type(element) == Room:
-            self.previousRoom = self.currentRoom
-            self.currentRoom = element
-        self.graph.addEdge(direction, (self.previousRoom, self.currentRoom))
-        #FIXME: in future assignments, an exit might not exist 
-        #in the opposite direction, so we should check for it before adding it
-        self.graph.addEdge(oppositeDir, (self.currentRoom, self.previousRoom))
-        self.graph.addEdges(self.currentRoom)
-        
+class RoomTests(unittest.TestCase):
+    '''Tests for the Room class'''
+
+    def setUp(self):
+        self.room1 = Room()
+        self.room1.setPurpose("dungeon")
+        self.room1.addCharacteristic("dark")
+        self.room1.addCharacteristic("dank")
+        self.room1.addCharacteristic("deep")
+        self.room1.addExit("north")
+
+        self.room2 = Room()
+        self.room2.setPurpose("dungeon")
+        self.room2.addCharacteristic("deep")
+        self.room2.addCharacteristic("dank")
+        self.room2.addCharacteristic("dark")
+        self.room2.addExit("north")
+
+    def testIsEqual(self):
+        result = self.room1.isEqual(self.room2)
+        self.assertEqual(result, True)
+
+        #Test against room characteristics
+        self.room1.addCharacteristic("smelly")
+        result = self.room1.isEqual(self.room2)
+        self.assertEqual(result, False)
+
+        self.room2.addCharacteristic("smelly")
+        result = self.room1.isEqual(self.room2)
+        self.assertEqual(result, True)
+
+        #Test against room purpose
+        self.room2.setPurpose("fountain")
+        result = self.room1.isEqual(self.room2)
+        self.assertEqual(result, False)
+
+# class GameOverTests(unittest.TestCase):
+    # def setUp(self):
+        # pass
+
 #FIXME: Unit tests needed
+
+if __name__ == "__main__":
+    unittest.main()
         
     
     
